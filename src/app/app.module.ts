27bf6d0d6f017e24ab8bb99ffc25760d4300a643
rubens.prado <109166405+Surfle@users.noreply.github.com>
2023-10-05 23:00:50 -0300

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './layout/index/index.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LoginComponent } from './sistema/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { PessoaslistComponent } from './pessoas/pessoaslist/pessoaslist.component';
+import { CarroslistComponent } from './carros/carroslist/carroslist.component';
+import { PessoasdetailsComponent } from './pessoas/pessoasdetails/pessoasdetails.component';
+import { CarrosdetailsComponent } from './carros/carrosdetails/carrosdetails.component';
+import { LivroslistComponent } from './livros/livroslist/livroslist.component';
+import { LivrosdetailsComponent } from './livros/livrosdetails/livrosdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    PessoaslistComponent,
+    CarroslistComponent,
+    PessoasdetailsComponent,
+    CarrosdetailsComponent,
+    LivroslistComponent,
+    LivrosdetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgbModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
